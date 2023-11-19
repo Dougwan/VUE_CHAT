def0da513ac5af733850af770fe52a3a5cb516f4
Douglas Louterio <douglaslouterio@Douglass-MacBook-Air.local>
2023-11-19 20:51:00 -0300

    remove tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
   <router-view />
 </template>
+
+<style>
+@import url("./style.css");
+</style>
--- src/pages/Chat.vue
+++ src/pages/Chat.vue
@@ -1,5 +1,20 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
 
-<template></template>
+const messages = ref([]);
+const message = ref("");
+</script>
+
+<template>
+  <section>
+    <div>
+      <textarea class="bg-red" id="messages" cols="30" rows="10"></textarea>
+
+      <p>{{ message }}</p>
+      <input v-model="message" type="text" />
+      <button>Enviar</button>
+    </div>
+  </section>
+</template>
 
 <style lang="scss" scoped></style>
